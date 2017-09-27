fecbc6d4a086d396b53bc7daa62af822e6b6d377
Christopher Hiller <boneskull@boneskull.com>
2017-09-26 22:53:15 -0700

    avoid port collision in http test

--- test/node-unit/http.spec.js
+++ test/node-unit/http.spec.js
@@ -1,16 +1,24 @@
 'use strict';
 
 var http = require('http');
+var getPort = require('get-port');
 
 var server = http.createServer(function (req, res) {
   res.end('Hello World\n');
 });
 
-server.listen(8888);
-
 describe('http', function () {
+  var port;
+
+  before(function () {
+    return getPort().then(function (p) {
+      port = p;
+      server.listen(port);
+    });
+  });
+
   it('should provide an example', function (done) {
-    http.get({ path: '/', port: 8888 }, function (res) {
+    http.get({ path: '/', port: port }, function (res) {
       expect(res).to.have.property('statusCode', 200);
       done();
     });
